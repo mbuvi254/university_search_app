0052d98624fea96693451c9b4d9287d831192348
mbuvi254 <danielmulatya95@gmail.com>
2025-11-15 18:35:12 +0300

    Updated styles for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,117 @@
-import axios from 'axios'
-import React, { useState,useEffect } from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import './App.css'
 
-interface University{
-  name:string,
-  web_pages:string[],
-  domains:string
-
+interface University {
+  name: string;
+  web_pages: string[];
+  domains: string[];
 }
 
-function App(){
-    const[country,setCounty]=useState("");
-    const handleChange =(e:React.ChangeEvent<HTMLInputElement>)=>{
-      setCounty(e.target.value);
+function App() {
+  const [country, setCounty] = useState("");
+  const [error, setError] = useState("");
+  const [uniCount, setUniCount] = useState<number>(0);
+  const [loading, setLoading] = useState(false);
+  const [selectedUrl, setSelectedUrl]=useState("")
+  const [showModal,setShowModal]=useState(false)
+  const [iframeLoading,setIframeLoading]=useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCounty(e.target.value);
+  };
+
+  const handleClick = () => {
+    handleGetUniversities();
+  };
+
+
+
+  const [universities, setUniversities] = useState<University[]>([]);
+  async function handleGetUniversities() {
+    setLoading(true);
+    setError("");
+    if (!country.trim()) {
+      setError("Please Provide a Country");
+      setLoading(false);
     }
-
-    const handleClick = ()=>{
-      handleGetUniversities();
+    try {
+      const response = await axios.get(
+        `http://universities.hipolabs.com/search?country=${country}`,
+      );
+      const universityData =response.data;
+      setUniversities(universityData);
+      if (universityData.length === 0) {
+      setError(`No Universities found in : ${country}`);
     }
-
-    const [universities , setUniversities]=useState<University[]>([]); 
-    async  function handleGetUniversities(){
-        try{
-        const response = await axios.get(`http://universities.hipolabs.com/search?country=${country}`)
-        setUniversities(response.data);
-        }catch(error){
-            console.log(error)
-
-        };
-}
- useEffect(()=>{
-      console.log(universities);
-      },[universities])
-return(
+    } catch (error) {
+      console.log(error);
+      setError("Failed getting universities");
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    setUniCount(universities.length)
+    //console.log(universities);
+  }, [universities]);
+  return (
     <>
     
-    <input placeholder='Enter Country' onChange={handleChange}   />
-    <button onClick={handleClick}>Get Universities</button>
+      <h2 className="appHeader">UNIVERSITY SEARCH</h2>
+
+    <section>
     
-    <ul>
-     {universities.map((university,index)=>(
-      <li key={index}>  
-      <a href={university.web_pages[0]}>
-        {university.name}
-        </a>
-      </li>
-     ))}
-     </ul>
+      <input value={country} type="text" className="searchInput" placeholder="Enter Country" onChange={handleChange} />
+      <button onClick={handleClick} disabled={loading} className="searchBtn">
+      <FaSearch />
+      </button>
+      </section>
+      <section>
+        {error && <p className="uniCount" style={{ color: "red" }}>{error}</p>}
+        {uniCount > 0 && (
+          <h3 className="uniCount">{uniCount} Universities in {country}</h3>
+        )}
+      </section>
+        {universities.map((university, index) => (
+          <div key={index}>
+            <div className="universityCard">
+            <div className="cardContainer">
+              <h4><b>{university.name}</b></h4>
+            <p><a href={university.web_pages[0]} target="_blank" rel="noopener noreferrer">{university.web_pages[0]}</a></p>
+             <button className="viewUniversityBtn" onClick={() => {
+                setSelectedUrl(university.web_pages[0]);
+                setIframeLoading(true); // start loader
+                setShowModal(true);
+              }}>
+                View
+              </button>
+            </div>
+            </div>
+          </div>
+        ))}
+   
+   <div className={`modalOverlay ${showModal ? "show" : ""}`}>
+  <div className="modalContent">
+    <button className="closeBtn" onClick={() =>{ 
+      setShowModal(false);
+      setIframeLoading(false);
+
+    }}>×</button>
+
+    <iframe
+      className="modalIframe"
+      src={selectedUrl}
+      title="University Website"
+    ></iframe>
+  </div>
+</div>
+
+
+      
     </>
-)
+  );
 }
 
 export default App;
